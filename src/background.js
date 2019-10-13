a32a7fccf5f6ff6512df4ee1faa49266942effa7
Chris Mitchell <cmitchell@crishell.co>
2019-10-13 08:44:54 -0600

    save

--- src/background.js
+++ src/background.js
@@ -1,42 +1,48 @@
 // import parseUrl from 'parse-github-url';
-const TOKEN_LOCAL_STORAGE_KEY = 'rms-pr-template-picker:token';
+const TOKEN_LOCAL_STORAGE_KEY = "rms-pr-template-picker:token";
 
-const composeContentsUrl = (owner, repo, path = '') => {
+const composeContentsUrl = (owner, repo, path = "") => {
   return `https://api.github.com/repos/${owner}/${repo}/contents/${path}`;
 };
 
-const getContents = ({ owner, repo, path = '', token }) => {
-  console.log('getContents', owner, repo, path, token);
+const getContents = ({ owner, repo, path = "", token }) => {
+  console.log("getContents", owner, repo, path, token);
   return fetch(composeContentsUrl(owner, repo, path), {
     headers: getRequestHeaders(token),
-    method: 'get',
+    method: "get"
   });
-}
+};
 
-const getRequestHeaders = (token) => {
+const getRequestHeaders = token => {
   return {
     Authorization: `token ${token}`,
-    Accept: 'application/vnd.github.v3+json'
+    Accept: "application/vnd.github.v3+json"
   };
-}
+};
 
-const getTemplates = async (data) => {
+const getTemplates = async data => {
   let secondaryTemplateFiles;
   const templates = [];
 
   // Get primary template
-  const primaryTemplateResponse = await getContents({ path: '.github/PULL_REQUEST_TEMPLATE.md', ...data });
+  const primaryTemplateResponse = await getContents({
+    path: ".github/PULL_REQUEST_TEMPLATE.md",
+    ...data
+  });
 
-  if(primaryTemplateResponse.ok) {
+  if (primaryTemplateResponse.ok) {
     const primaryTemplate = await primaryTemplateResponse.json();
     primaryTemplate.content = atob(primaryTemplate.content);
     templates.push(primaryTemplate);
   }
 
   // Get secondary templates
-  secondaryTemplateFiles = await getContents({ path: '.github/PULL_REQUEST_TEMPLATE', ...data });
+  secondaryTemplateFiles = await getContents({
+    path: ".github/PULL_REQUEST_TEMPLATE",
+    ...data
+  });
 
-  if(!secondaryTemplateFiles.ok) {
+  if (!secondaryTemplateFiles.ok) {
     return templates;
   }
 
@@ -45,89 +51,99 @@ const getTemplates = async (data) => {
   let secondaryTemplateResponse;
   let secondaryTemplate;
 
-  for(i in secondaryTemplateFileContents) {
-    secondaryTemplateResponse = await getContents({ path: secondaryTemplateFileContents[i].path, ...data });
+  for (i in secondaryTemplateFileContents) {
+    secondaryTemplateResponse = await getContents({
+      path: secondaryTemplateFileContents[i].path,
+      ...data
+    });
     secondaryTemplate = await secondaryTemplateResponse.json();
     secondaryTemplate.content = atob(secondaryTemplate.content);
     templates.push(secondaryTemplate);
   }
 
   return templates;
-}
+};
 
-const postMessage = (message) => {
-  const port = chrome.runtime.connect({ name: 'template-api' });
+const postMessage = message => {
+  const port = chrome.runtime.connect({ name: "template-api" });
 
   port.postMessage(message);
-}
+};
 
 const testAndSetToken = async (token, port) => {
-  const response = await fetch('https://api.github.com/user', {
+  const response = await fetch("https://api.github.com/user", {
     headers: getRequestHeaders(token),
-    method: 'get'
+    method: "get"
   });
 
-  if(response.ok) {
+  if (response.ok) {
     chrome.storage.sync.set({ token: token });
-    port.postMessage({type: 'token_valid', data: token});
+    port.postMessage({ type: "token_valid", data: token });
   } else {
-    chrome.storage.sync.remove('token');
-    port.postMessage({type: 'token_invalid'});
+    chrome.storage.sync.remove("token");
+    port.postMessage({ type: "token_invalid" });
   }
-}
+};
 
 function parseUrl(url) {
-  const pieces = url.split('/');
+  const pieces = url.split("/");
   return {
     owner: pieces[3],
     repo: pieces[4]
-  }
+  };
 }
 
 chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
-  console.log(arguments);
+  var gitHubCommit = /.*github.com\/.*\/compare\/.*/;
+
+  if (
+    changeInfo.status === "complete" &&
+    tab.status === "complete" &&
+    gitHubCommit.test(tab.url)
+  ) {
+    console.log("onActivated passed");
+    sendMsgToActiveTab({ type: "onCompare" });
+  }
 });
 
-// chrome.runtime.onInstalled.addListener(() => {
-//   chrome.declarativeContent.onPageChanged.removeRules(undefined, () => {
-//     chrome.declarativeContent.onPageChanged.addRules([{
-//       conditions: [new chrome.declarativeContent.PageStateMatcher({
-//         pageUrl: {hostEquals: 'github.com'},
-//       })],
-//       actions: [new chrome.declarativeContent.ShowPageAction()]
-//     }]);
-//   });
-// });
+function sendMsgToActiveTab(message) {
+  chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
+    chrome.tabs.sendMessage(tabs[0].id, message, function(response) {
+      console.log(response);
+    });
+  });
+}
 
-chrome.runtime.onConnect.addListener((port) => {
+chrome.runtime.onConnect.addListener(port => {
   port.onMessage.addListener(({ type, data }) => {
-    switch(type) {
-      case 'ready':
-        chrome.storage.sync.get('token', async ({ token }) => {
+    switch (type) {
+      case "ready":
+        console.log("b ready");
+        chrome.storage.sync.get("token", async ({ token }) => {
           port.postMessage({
-            type: 'token',
+            type: "token",
             data: token
-          })
+          });
 
           console.log(data, parseUrl(data));
 
           const templates = await getTemplates({
             token,
             ...parseUrl(data)
-          })
+          });
 
           console.log(templates);
 
           port.postMessage({
-            type: 'templates',
+            type: "templates",
             data: templates
-          })
+          });
         });
         break;
-      case 'remove_token':
-        chrome.storage.sync.remove('token');
+      case "remove_token":
+        chrome.storage.sync.remove("token");
         break;
-      case 'set_token':
+      case "set_token":
         testAndSetToken(data, port);
         break;
     }
--- src/content.js
+++ src/content.js
@@ -1,21 +1,40 @@
-import App from './content/App.svelte';
+import App from "./content/App.svelte";
 
-const target = document.querySelector('[data-project-hovercards-enabled]');
-const app = new App({
-	anchor: target.firstChild,
-	target,
+function setup() {
+  console.log("setup");
+  const target = document.querySelector("[data-project-hovercards-enabled]");
+
+  if (!target) {
+    return;
+  }
+  console.log("content");
+
+  const app = new App({
+    anchor: target.firstChild,
+    target
+  });
+
+  app.port.onMessage.addListener(function({ type, data }) {
+    switch (type) {
+      case "templates":
+        app.$set({ templates: data });
+        break;
+      default:
+        break;
+    }
+  });
+
+  app.postMessage({ type: "ready", data: window.location.href });
+}
+
+chrome.runtime.onMessage.addListener(function({ type }, sender, sendResponse) {
+  switch (type) {
+    case "onCompare":
+      setup();
+      // sendResponse('onCompare received');
+      // app.postMessage({ type: 'ready', data: window.location.href });
+      break;
+  }
 });
 
-app.port.onMessage.addListener(function({type, data}) {
-	switch(type) {
-		case 'templates':
-			app.$set({ templates: data });
-			break;
-		default:
-			break;
-	}
-});
-
-app.postMessage({ type: 'ready', data: window.location.href });
-
-export default app;
+// export default app;
